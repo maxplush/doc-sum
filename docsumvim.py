b81ff7291ab548da096dff8169f9ef7066e4bd4c
maxplush <maxabeplush@gmail.com>
2024-09-04 14:26:58 -0700

    Summarize now all documents, u to class two pace

--- docsumvim.py
+++ docsumvim.py
@@ -1,19 +1,35 @@
 import os
 from groq import Groq
+import argparse
+
+
+# Parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+
 
 client = Groq(
     # This is the default and can be omitted
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
+with open(args.filename) as f:
+	 text = f.read()
+
 chat_completion = client.chat.completions.create(
     messages=[
         {
+            "role": "system",
+            "content": "Summarize the text below. Limit it to 1 paragraph, at a first grade reading level.",
+        },
+        {
             "role": "user",
-            "content": "Explain the importance of low latency LLMs",
+            "content": text,
         }
     ],
     model="llama3-8b-8192",
 )
+
 print(chat_completion.choices[0].message.content)
 
