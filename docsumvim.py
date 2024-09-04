d3da856ad73477bf9c815cf404096ab60b018291
maxplush <maxabeplush@gmail.com>
2024-09-04 14:11:14 -0700

    Added updated docs

---
+++ docsumvim.py
@@ -0,0 +1,19 @@
+import os
+from groq import Groq
+
+client = Groq(
+    # This is the default and can be omitted
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of low latency LLMs",
+        }
+    ],
+    model="llama3-8b-8192",
+)
+print(chat_completion.choices[0].message.content)
+
